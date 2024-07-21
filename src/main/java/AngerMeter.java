a8a50a8dc35f8502e1d8bb6534ad5ab70fbc315c
localmantony <115189163+localmantony@users.noreply.github.com>
2024-07-21 03:12:22 +0000

    testing arrays, separated AngerMeter.java, idea for touchstone: pizza reviewer

---
+++ src/main/java/AngerMeter.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AngerMeter {
+	public static void main(String[] args) {
+		Scanner userInput = new Scanner(System.in);
+		int anger = 5;
+		while (anger < 10) {
+			System.out.println("Simple test. Please ignore and do not type any numbers");
+			try {
+				int mult = userInput.nextInt();
+				System.out.println("I'm getting angrier. Please STOP typing numbers.");
+				anger++;
+
+			}
+			catch (Exception ex) {
+				System.out.println("You sure you don't want to type any numbers?");
+				int mult = userInput.nextInt();
+			}
+		}		
+	}
+}
